9d2daad7f881914b9b0b3d7efaa64ce7b1ded4a5
seno <seno.harist@outlook.com>
2023-06-02 10:35:43 +0700

    make app.py to joined endpoint

--- app.py
+++ app.py
@@ -1,159 +1,162 @@
-# inisialisasi library
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_cors import CORS
+from flask import Flask, make_response, jsonify, request
+import dataset
 
-# import library flask sqlalchemy
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-# inisialisasi object library
 app = Flask(__name__)
+db = dataset.connect('sqlite:///api.db')
 
-# inisiasai objek flask restful
-api = Api(app)
-
-# inisiasi object flask cors
-CORS(app)
-
-# inisialisasi object flask sqlalchemy
-db = SQLAlchemy(app)
-
-# mongkonfigurasi dulu database
-basedir = os.path.dirname(os.path.abspath(__file__))
-database = "sqlite:///" + os.path.join(basedir, "db.sqlite")
-app.config["SQLALCHEMY_DATABASE_URI"] = database
-
-# Membuat database model
-class ModelDatabase(db.Model):
-    # membuat field/kolom
-    id = db.Column(db.Integer, primary_key=True)
-    nama = db.Column(db.String(100))
-    umur = db.Column(db.Integer)
-    alamat = db.Column(db.TEXT) # field tambahan
-
-    # membuat mothode untuk menyimpan data agar lebih simple
-    def save(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-            return True
-        except:
-            return False
+# table = db['books']
+tabled = db['dmss']
 
 
-# mencreate database
-db.create_all()
-
-# inisiasi variabel kosong bertipe dictionary
-identitas = {}
-
-# Membuat class untuk restfull 
-class ContohResource(Resource):
-    def get(self):
-        # menampilkan data dari database sqlite
-        query = ModelDatabase.query.all()
-
-        # melakukan iterasi pada modelDatabase dengan teknik 
-        output = [
-            {
-                "id":data.id,
-                "nama":data.nama, 
-                "umur":data.umur, 
-                "alamat":data.alamat
-            } 
-            for data in query
-        ]
-
-        response = {
-            "code" : 200, 
-            "msg"  : "Query data sukses",
-            "data" : output
-        }
-
-        return response, 200
-
-    def post(self):
-        dataNama = request.form["nama"]
-        dataUmur = request.form["umur"]
-        dataAlamat = request.form["alamat"]
-
-        # masukan data ke dalam database model
-        model = ModelDatabase(nama=dataNama, umur=dataUmur, alamat=dataAlamat)
-        model.save()
-         
-        response = {
-            "msg" : "Data berhasil dimasukan",
-            "code": 200
-        }
-
-        return response, 200
-
-    # delete all / hapus semua datanya
-    def delete(self):
-        # query all data
-        query = ModelDatabase.query.all() # list / kumpulan data => iterasi/looping
-
-        # looping
-        for data in query:
-            db.session.delete(data)
-            db.session.commit()
-
-        response = {
-            "msg":"Semua data berhasil dihapus",
-            "code":200
-        }
-
-        return response, 200
-        
+def fetch_dbd(dist_code):  # Each dms scnerio
+    return tabled.find_one(dist_code=dist_code)
 
 
-# membuat class baru untuk mengedit / menghapus data
-class UpdateResource(Resource):
-    def put(self, id):
-        # konsumsi id itu untuk query di model databasenya
-        # pilih data yang ingin diedit berdasarkan id yang dimasukan
-        query = ModelDatabase.query.get(id)
-
-        # form untuk pegeditan data
-        editNama = request.form["nama"]
-        editUmur = request.form["umur"]
-        editAlamat = request.form["alamat"]
-
-        # mereplace nilai yang ada di setiap field/kolom
-        query.nama = editNama
-        query.umur = editUmur
-        query.alamat = editAlamat
-        db.session.commit()
-
-        response = {
-            "msg" : "edit data berhasil",
-            "code": 200
-        }
-
-        return response, 200
-
-    # delete by id, bukan delete all
-    def delete(self, id):
-        queryData = ModelDatabase.query.get(id)
-
-        # panggil methode untuk delete data by id
-        db.session.delete(queryData)
-        db.session.commit()
-
-        response = {
-            "msg" : "delete data berhasil",
-            "code" : 200
-        }
-        return response, 200
+def fetch_dbd_all():
+    dmss = []
+    for dms in tabled:
+        dmss.append(dms)
+    return dmss
 
 
-# inisialisasi url / api 
-# testing
-api.add_resource(ContohResource, "/api", methods=["GET", "POST", "DELETE"])
-api.add_resource(UpdateResource, "/api/<id>", methods=["PUT", "DELETE"])
+@app.route('/api/dbd_populate', methods=['GET'])
+def dbd_populate():
+    tabled.insert({
+        "dist_code": "443501",
+        "dist_name": "MUSS jombang",
+        "depo_code": "3344",
+        "depo_name": "depo surabaya",
+        "dms_email": "akbar",
+        "wa_no": "081122334455"
+    })
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5005)
-    
+    tabled.insert({
+        "dist_code": "440101",
+        "dist_name": "Affandison Gresik",
+        "depo_code": "3243",
+        "depo_name": "depo surabaya",
+        "dms_email": "akbar",
+        "wa_no": "085566778899"
+    })
 
+    return make_response(jsonify(fetch_dbd_all()),
+                         200)
+
+
+@app.route('/api/dms', methods=['GET', 'POST'])
+def api_dmss():
+    if request.method == "GET":
+        return make_response(jsonify(fetch_dbd_all()), 200)
+    elif request.method == 'POST':
+        content = request.json
+        dist_code = content['dist_code']
+        tabled.insert(content)
+        return make_response(jsonify(fetch_dbd(dist_code), 201))  # 201 = Created
+
+
+@app.route('/api/dms/<dist_code>', methods=['GET', 'PUT', 'DELETE'])
+def api_each_dms(dist_code):
+    if request.method == "GET":
+        dms_obj = fetch_dbd(dist_code)
+        if dms_obj:
+            return make_response(jsonify(dms_obj), 200)
+        else:
+            return make_response(jsonify(dms_obj), 404)
+    elif request.method == "PUT":  # Updates the book
+        content = request.json
+        tabled.update(content, ['dist_code'])
+
+        dms_obj = fetch_dbd(dist_code)
+        return make_response(jsonify(dms_obj), 200)
+    elif request.method == "DELETE":
+        tabled.delete(id=dist_code)
+
+        return make_response(jsonify({}), 204)
+
+
+
+
+'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+from flask import Flask, make_response, jsonify, request
+import dataset
+
+app = Flask(__name__)
+db = dataset.connect('sqlite:///api.db')
+
+'''
+# table = db['books']
+table = db['traffics']
+
+
+def fetch_db(wa_no):  # Each book scnerio
+    return table.find_one(wa_no=wa_no)
+
+
+def fetch_db_all():
+    traffics = []
+    for wac in table:
+        traffics.append(wac)
+    return traffics
+
+
+@app.route('/api/db_populated', methods=['GET'])
+def db_populated():
+    table.insert({
+        "dist_email": "443501@mailinator.com",
+        "dist_name": "MUSS jombang",
+        "question": "tampilan sahabat warung tidak seperti biasanya",
+        "dist_code": "depo surabaya",
+        "product": "B2B",
+        "wa_no": "081122334455"
+    })
+
+    table.insert({
+        "dist_email": "440101@mailinator.com",
+        "dist_name": "Affandison Gresik",
+        "question": "kenapa saya tidak bisa login pagi ini ",
+        "dist_code": "depo surabaya",
+        "product": "OSDP",
+        "wa_no": "085566778899"
+    })
+
+    return make_response(jsonify(fetch_db_all()),
+                         200)
+
+
+@app.route('/api/wac', methods=['GET', 'POST'])
+def api_traffics():
+    if request.method == "GET":
+        return make_response(jsonify(fetch_db_all()), 200)
+    elif request.method == 'POST':
+        content = request.json
+        wa_no = content['wa_no']
+        table.insert(content)
+        return make_response(jsonify(fetch_db(wa_no), 201))  # 201 = Created
+
+
+@app.route('/api/wac/<wa_no>', methods=['GET', 'PUT', 'DELETE'])
+def api_each_wa(wa_no):
+    if request.method == "GET":
+        wac_obj = fetch_db(wa_no)
+        if wac_obj:
+            return make_response(jsonify(wac_obj), 200)
+        else:
+            return make_response(jsonify(wac_obj), 404)
+    elif request.method == "PUT":  # Updates the book
+        content = request.json
+        table.update(content, ['wa_no'])
+
+        wac_obj = fetch_db(wa_no)
+        return make_response(jsonify(wac_obj), 200)
+    elif request.method == "DELETE":
+        table.delete(id=wa_no)
+
+        return make_response(jsonify({}), 204)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
